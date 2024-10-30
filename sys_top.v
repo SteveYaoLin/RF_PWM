7045c473efd04d7a268eb8c40ac4f18bdf5d4bad
姚琳 <yaolin0616@163.com>
2024-10-30 09:53:20 +0800

    生成MCS文件，输出PIn设置FAST和16mA。波形上看没什么用。top上一个net脉冲给两个ODDR输出。

--- sys_top.v
+++ sys_top.v
@@ -47,6 +47,11 @@ module sys_top # (parameter _RAM_WIDTH = 32)
 
     wire pulse_valid;
 
+    wire pulseOut_a;
+    wire pulseOut_b;
+    wire pulseOut_c;
+    wire pulseOut_d;
+
     clk_wiz_0 u_pll
     (
 
@@ -87,20 +92,112 @@ module sys_top # (parameter _RAM_WIDTH = 32)
     .io_rst(io_rst),
     .io_en(pulse_begin|pulse_valid),
     .pwm_dis(pwm_dis),
-    .io_pulseOut_a(io_pulseOut_a),
-    .io_pulseOut_b(io_pulseOut_b),
+    .io_pulseOut_a(pulseOut_a),
+    .io_pulseOut_b(pulseOut_b),
     .die_period(die_period),
     .pulse_period(pulse_period),
     .io_defaultLevel(io_defaultLevel),
     .pulse_valid(pulse_valid)
   );
   
-  
-	assign io_pulseOut_c = 0;
-	assign io_pulseOut_d = 0;
+//    assign io_pulseOut_a    = pulseOut_a;
+//    assign io_pulseOut_b    = pulseOut_b;
+//	assign io_pulseOut_c = pulseOut_a;
+//	assign io_pulseOut_d = pulseOut_b;
 	
 	assign die_period = 3;
 	assign pulse_period = 10;
+
+ODDR2 #(
+   .DDR_ALIGNMENT("NONE"),
+   .INIT(1'b0),
+   .SRTYPE("SYNC")
+) ODDR_inst_a (
+   .Q(io_pulseOut_a),
+   .C0(pulseOut_a),
+   .C1(~pulseOut_a),
+   .CE(1'b1),
+   .D0(1'b1),
+   .D1(1'b0),
+   .R(1'b0),
+   .S(1'b0)
+);
+
+ODDR2 #(
+   .DDR_ALIGNMENT("NONE"),
+   .INIT(1'b0),
+   .SRTYPE("SYNC")
+) ODDR_inst_b (
+   .Q(io_pulseOut_b),
+   .C0(pulseOut_b),
+   .C1(~pulseOut_b),
+   .CE(1'b1),
+   .D0(1'b1),
+   .D1(1'b0),
+   .R(1'b0),
+   .S(1'b0)
+);
+
+ODDR2 #(
+   .DDR_ALIGNMENT("NONE"),
+   .INIT(1'b0),
+   .SRTYPE("SYNC")
+) ODDR_inst_c (
+   .Q(io_pulseOut_c),
+   .C0(pulseOut_b),
+   .C1(~pulseOut_b),
+   .CE(1'b1),
+   .D0(1'b1),
+   .D1(1'b0),
+   .R(1'b0),
+   .S(1'b0)
+);
+
+ODDR2 #(
+   .DDR_ALIGNMENT("NONE"),
+   .INIT(1'b0),
+   .SRTYPE("SYNC")
+) ODDR_inst_d (
+   .Q(io_pulseOut_d),
+   .C0(pulseOut_a),
+   .C1(~pulseOut_a),
+   .CE(1'b1),
+   .D0(1'b1),
+   .D1(1'b0),
+   .R(1'b0),
+   .S(1'b0)
+);
+
+
+
+    // // 使用 IODELAY2 来实现 2ns 延迟
+    // IODELAY2 #(
+    //     .IDELAY_VALUE(2),      // 延迟的初始值
+    //     .IDELAY_TYPE("FIXED"), // 固定延迟类型
+    //     .DELAY_SRC("I")        // 延迟源为输入信号
+    // ) IODELAY2_instd (
+    //     .IDATAIN(pulseOut_a),           // 输入数据
+    //     .ODATAIN(1'b0),        // 不使用输出数据路径
+    //     .DATAOUT(io_pulseOut_d),   // 延迟后的输出
+    //     .T(1'b1),              // 设置为输入模式
+    //     .ODATAIN(1'b0),        // 不使用输出数据路径
+    //     .C(io_clk)                // 时钟信号
+    // );
+
+    //     // 使用 IODELAY2 来实现 2ns 延迟
+    // IODELAY2 #(
+    //     .IDELAY_VALUE(2),      // 延迟的初始值
+    //     .IDELAY_TYPE("FIXED"), // 固定延迟类型
+    //     .DELAY_SRC("I")        // 延迟源为输入信号
+    // ) IODELAY2_instc (
+    //     .IDATAIN(pulseOut_b),           // 输入数据
+    //     .ODATAIN(1'b0),        // 不使用输出数据路径
+    //     .DATAOUT(io_pulseOut_c),   // 延迟后的输出
+    //     .T(1'b1),              // 设置为输入模式
+    //     .ODATAIN(1'b0),        // 不使用输出数据路径
+    //     .C(io_clk)                // 时钟信号
+    // );
+
 	
 	reg[24:0] counter = 0;//变量led_out 定义为寄存器型
 	reg[7:0] sig_led = 0;
@@ -117,4 +214,5 @@ begin
 	end
 	
 end
+
 endmodule
